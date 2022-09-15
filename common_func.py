424bc76b8d19ce7cb99cb15f1fa3c303a13bdb05
Wayne <waynela15045@gmail.com>
2022-09-16 06:05:06 +0800

    v1

---
+++ common_func.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+
+
+# function gets the lat and lon details from the given address
+# its a free API with a rate limit of 250 per min
+
+
+def getcoordinates(address):
+    req = requests.get('https://developers.onemap.sg/commonapi/search?searchVal='+address+'&returnGeom=Y&getAddrDetails=Y&pageNum=1')
+    resultsdict = eval(req.text)
+    if len(resultsdict['results'])>0:
+        return [resultsdict['results'][0]['LATITUDE'], resultsdict['results'][0]['LONGITUDE']]
+    else:
+        pass
+
+def create_lat_lon(new_df):
+    new_df = new_df.join(pd.DataFrame(new_df['Coords'].values.tolist(),columns=['LAT','LONG']),on=new_df.index)
+    new_df.set_index(new_df.columns[0],inplace=True)
+    new_df.drop(columns=['Coords'],inplace=True)
+    return new_df
+#%%
