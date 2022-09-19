55d2d597b002ac213695de250af0a49552bedc08
Wayne <waynela15045@gmail.com>
2022-09-19 11:06:18 +0800

    amend

--- common_func.py
+++ common_func.py
@@ -1,7 +1,8 @@
 import requests
 import pandas as pd
 import numpy as np
-
+import difflib as dl
+from datetime import datetime
 
 # function gets the lat and lon details from the given address
 # it's a free API with a rate limit of 250 per min
@@ -31,6 +32,39 @@ def load_mrt_malls():
     convert_malls_array = malls[['LAT', 'LONG']].to_numpy()
     return convert_mrt_array, convert_malls_array
 
+
+def info_parser(features: dict, mrt, malls, hdb_details_df: pd.DataFrame):
+    postal = features['Postal']
+    req = requests.get('https://developers.onemap.sg/commonapi/search?searchVal=' + str(
+        postal) + '&returnGeom=Y&getAddrDetails=Y&pageNum=1')
+    resultsdict = eval(req.text)
+    if len(resultsdict['results']) > 0:
+        blk, street = resultsdict['results'][0]['BLK_NO'], resultsdict['results'][0]['ROAD_NAME']
+
+        ignore_short_forms = dl.get_close_matches(street, hdb_details_df['street'].values)[0]
+
+        row = hdb_details_df[(hdb_details_df['blk_no'] == blk) & (hdb_details_df['street'] == ignore_short_forms)]
+
+        # build dict of features
+        features['year'] = datetime.now().year
+        if features['Lease'] == '':
+            features['remaining_lease_year'] = features['year'] - row.iloc[0, -1]
+        else:
+            features['remaining_lease_year'] = features['year'] - int(features['Lease'])
+
+        if features['floor_area_sqm'] == '':
+            features['floor_area_sqm'] = avg_area[int(features['flat_cat'])]
+
+        coords = np.array([float(resultsdict['results'][0]['LATITUDE']),
+                           float(resultsdict['results'][0]['LONGITUDE'])])
+        features['distance_to_town'] = np.linalg.norm(coords - np.array([1.300556, 103.821667])) * conversion
+        features['distance_to_mrt'] = min(np.linalg.norm(coords - mrt_array, axis=1)) * conversion
+        features['distance_to_mall'] = min(np.linalg.norm(coords - malls_array, axis=1)) * conversion
+        features['number_of_malls'] = (np.linalg.norm(coords - malls_array, axis=1) < radius / conversion).sum()
+
+        final_features = np.array([features[i] for i in reorder_list],
+                                  dtype=object).reshape(1, len(reorder_list))
+        return final_features
 
 # helper variables for calculations
 
