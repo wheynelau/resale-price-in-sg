cbe3a0621f4b61b9546a8e55e091cf2151fa34f3
wheynelau <waynelau15045@gmail.com>
2023-09-21 22:20:20 +0800

    feat: hide settings

--- pages/3_Search_nearby.py
+++ pages/3_Search_nearby.py
@@ -12,7 +12,7 @@ def load_csv():
 if "data" not in st.session_state:
     st.session_state.data = load_csv()
 
-address = st.text_input("Enter your address", "Blk 1 Jalan Bukit Merah")
+address = st.text_input("Enter your address", placeholder="Enter your address or postal code")
 
 if address:
     lat,lon = getcoordinates(address)
@@ -23,19 +23,30 @@ else:
     results = pd.DataFrame()
 
 if not results.empty:
-    lease_range = st.slider('Lease commencement date', min(results['lease_commence_date']), 
-                            max(results['lease_commence_date']),
-                             (min(results['lease_commence_date']), 
-                              max(results['lease_commence_date'])))
-    column2_value = st.selectbox('Choose flat type value:', options=results['flat_type'].unique())
-    distance = st.slider('Radius in KM', 0.05,1.0, step = 0.05)
+    flat_type = st.selectbox('Choose flat type value:', options=results['flat_type'].unique())
+    with st.expander("Additional settings", expanded=False):
+        lease_range = st.slider('Lease commencement date', min(results['lease_commence_date']), 
+                                max(results['lease_commence_date']),
+                                (min(results['lease_commence_date']), 
+                                max(results['lease_commence_date'])),
+                                help = "Select the range of lease commencement date")
+
+
+        distance = st.slider('Radius in KM', 0.05,1.0,value = 1.0, step = 0.05,
+                             help="This is just an estimate.")
 
     # Applying filters
     filtered_results = results[results['distance'] <= distance]
     filtered_results = filtered_results[(filtered_results['lease_commence_date'] >= lease_range[0]) &
                                          (filtered_results['lease_commence_date'] <= lease_range[1])]
-    filtered_results = filtered_results[filtered_results['flat_type'] == column2_value]
+    filtered_results = filtered_results[filtered_results['flat_type'] ==flat_type]
     filtered_results['month'] = filtered_results['year'].astype(str) + '-' + filtered_results['month'].astype(str)
     filtered_results['lease_commence_date'] = filtered_results['lease_commence_date'].astype(str)
-
-    st.write(filtered_results[['month','lease_commence_date','remaining_lease_year','storey_range','floor_area_sqm','resale_price','address']].reset_index(drop=True))
+    st.markdown("""### Results
+                The columns can be sorted by clicking on the column name.
+                """)
+    st.dataframe(filtered_results[['month','lease_commence_date','remaining_lease_year',
+                               'storey_range','floor_area_sqm','resale_price','address',
+                               'distance'
+                               ]]
+                               .head(100), hide_index=True)
