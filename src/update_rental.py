caab951fe2ced7d223f98fcae1a64ac5f5087981
wheynelau <waynelau15045@gmail.com>
2023-10-10 16:14:56 +0800

    chore: install precommit

---
+++ src/update_rental.py
@@ -0,0 +1,90 @@
+import sys
+import time
+import pandas as pd
+import requests
+from utils.preprocessing import Preprocessor
+
+
+class RentalDataset:
+
+    """
+    A class for fetching JSON data from the HDB resale price dataset API.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the HDBDataset class.
+        """
+        self._resource_id = self.get_first_dataset()
+
+    def fetch_json_response(self, **params):
+        """
+        Fetches JSON data from a given URL with the specified parameters.
+
+        Args:
+            params (dict, optional): Dictionary of query parameters.
+
+        Returns:
+            dict: JSON data as a Python dictionary.
+        """
+        if params.get("resource_id", None) is None:
+            params["resource_id"] = self._resource_id
+        response = requests.get(
+            "https://data.gov.sg/api/action/datastore_search", params=params, timeout=5
+        )
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            return response.json()["result"]["records"]
+        response.raise_for_status()
+
+    def get_first_dataset(self):
+        """
+        Gets the resource ID of the first dataset that contains data from January 2017.
+
+        Returns:
+            str: The resource ID of the first dataset that contains data from January 2017.
+        """
+        metadata = requests.get(
+            "https://api-production.data.gov.sg/v2/public/api/collections/166/metadata",
+            timeout=5,
+        )
+        return metadata.json()["data"]["collectionMetadata"]["childDatasets"][0]
+
+
+if __name__ == "__main__":
+    dataset = RentalDataset()
+    preprocessor = Preprocessor()
+
+    CSV_PATH = "assets/data/test_rental_flats.csv"
+
+    # 1. Load the current data
+    old_df = pd.read_csv(CSV_PATH, index_col=0)
+
+    # 2. Fetch the new data
+
+    # 1000 is usually safe, it will usually not return more than 1000 records
+    sample = dataset.fetch_json_response(limit=1000, offset=len(old_df))
+
+    # 2a. Check if the new data is empty
+
+    if len(sample) == 0:
+
+        print("No new data to update")
+        sys.exit(0)
+
+    # 3. Convert the new data to a DataFrame
+    print(f"New data fetched successfully, num differences = {len(sample)}")
+    new_data = pd.DataFrame(sample)
+    new_data["_id"] = new_data["_id"] - 1
+    new_data.set_index("_id", inplace=True)
+
+    # 4. Preprocess the new data
+    preprocessor.get_address(new_data)
+
+    # 5. Merge the new data with the old data
+    appended = preprocessor.add_new_to_old(old_df, new_data)
+
+    # 6. Save the merged data
+
+    appended.to_csv(CSV_PATH)
--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,36 +1,39 @@
 import pandas as pd
 import numpy as np
+import json
 import requests
 import difflib as dl
 from datetime import datetime
+from tqdm import tqdm
 
 
 def load_mrt_malls():
-    mrt = pd.read_csv('assets/amenities/mrt.csv')
-    malls = pd.read_csv('assets/amenities/malls.csv')
-    convert_mrt_array = mrt[['LAT', 'LONG']].to_numpy()
-    convert_malls_array = malls[['LAT', 'LONG']].to_numpy()
+    mrt = pd.read_csv("assets/amenities/mrt.csv")
+    malls = pd.read_csv("assets/amenities/malls.csv")
+    convert_mrt_array = mrt[["LAT", "LONG"]].to_numpy()
+    convert_malls_array = malls[["LAT", "LONG"]].to_numpy()
     return convert_mrt_array, convert_malls_array
+
 
 class Preprocessor:
     """
-    A class for preprocessing the HDB resale price dataset.
+    A class for preprocessing the common HDB dataset.
 
     Methods:
     --------
     get_address(df):
-        Adds a new column 'address' to the dataframe by concatenating 'block' and 
+        Adds a new column 'address' to the dataframe by concatenating 'block' and
         'street_name' columns and drops the original columns.
 
     getcoordinates(address):
         Returns the latitude and longitude of the given address using OneMap API.
 
     get_storey_range(df):
-        Splits the 'storey_range' column into two columns and takes the mean of the 
+        Splits the 'storey_range' column into two columns and takes the mean of the
         two values. Replaces the original column with the mean value.
 
     get_remaining_lease(features):
-        Calculates the remaining lease year of each flat based on the 'month' and 
+        Calculates the remaining lease year of each flat based on the 'month' and
         'lease_commence_date' columns.
 
     get_lat_lon(old_df:pd.DataFrame, new_df):
@@ -38,7 +41,7 @@ class Preprocessor:
           values from the old dataframe or by calling the getcoordinates method.
 
     set_dtypes(df):
-        Converts the 'lease_commence_date' column to datetime and 'resale_price' and 
+        Converts the 'lease_commence_date' column to datetime and 'resale_price' and
         'floor_area_sqm' columns to float32.
 
     create_year_month(df):
@@ -51,68 +54,87 @@ class Preprocessor:
     __call__(self, df:pd.DataFrame):
         Calls all the above methods to preprocess the dataframe.
     """
+
     @staticmethod
     def get_address(df):
-        df['address'] = df['block'] + ' ' + df['street_name']
-        df.drop(['block', 'street_name'], axis=1, inplace=True)
+        df["address"] = df["block"] + " " + df["street_name"]
+        df.drop(["block", "street_name"], axis=1, inplace=True)
 
     @staticmethod
     def getcoordinates(address):
         req = requests.get(
-            'https://developers.onemap.sg/commonapi/search?searchVal=' +
-              address +
-                '&returnGeom=Y&getAddrDetails=Y&pageNum=1', timeout=30)
-        resultsdict = eval(req.text)
-        if len(resultsdict['results']) > 0:
-            return [resultsdict['results'][0]['LATITUDE'], resultsdict['results'][0]['LONGITUDE']]
+            "https://developers.onemap.sg/commonapi/search?searchVal="
+            + address
+            + "&returnGeom=Y&getAddrDetails=Y&pageNum=1",
+            timeout=30,
+        )
+        resultsdict = json.loads(req.text)
+        if len(resultsdict["results"]) > 0:
+            return [
+                resultsdict["results"][0]["LATITUDE"],
+                resultsdict["results"][0]["LONGITUDE"],
+            ]
         else:
             pass
 
     @staticmethod
     def get_storey_range(df):
-        df_storey =df['storey_range'].str.split(pat = " TO ", expand=True)
+        df_storey = df["storey_range"].str.split(pat=" TO ", expand=True)
         df_storey = df_storey.astype(int)
         df_storey.head()
-        df['storey_range'] =  df_storey.mean(axis = 1)
+        df["storey_range"] = df_storey.mean(axis=1)
 
     @staticmethod
     def get_remaining_lease(features):
-        features['month'] = pd.to_datetime(features['month'],format="%Y-%m")
-        features['current_lease_dt']=features['month']-pd.to_datetime(features['lease_commence_date'],format="%Y")
-        features['current_lease_year'] = features['current_lease_dt'] / np.timedelta64(1, 'Y')
-        features['remaining_lease_year'] = 99-features['current_lease_year']
-        features['remaining_lease_year'] = features['remaining_lease_year'].round(2)
-        features.drop(columns = ['current_lease_year','current_lease_dt']
-                    ,axis=1,inplace=True)
+        features["month"] = pd.to_datetime(features["month"], format="%Y-%m")
+        features["current_lease_dt"] = features["month"] - pd.to_datetime(
+            features["lease_commence_date"], format="%Y"
+        )
+        features["current_lease_year"] = features["current_lease_dt"] / np.timedelta64(
+            1, "Y"
+        )
+        features["remaining_lease_year"] = 99 - features["current_lease_year"]
+        features["remaining_lease_year"] = features["remaining_lease_year"].round(2)
+        features.drop(
+            columns=["current_lease_year", "current_lease_dt"], axis=1, inplace=True
+        )
 
-    def get_lat_lon(self,old_df:pd.DataFrame, new_df):
-        dict_lat_lon = old_df[['address','latitude','longitude']].drop_duplicates(subset=['address']).set_index('address').to_dict()
-        for idx,row in new_df.iterrows():
-            value = dict_lat_lon['latitude'].get(row['address'], np.nan)
+    def get_lat_lon(self, old_df: pd.DataFrame, new_df: pd.DataFrame = None):
+        dict_lat_lon = (
+            old_df[["address", "latitude", "longitude"]]
+            .drop_duplicates(subset=["address"])
+            .set_index("address")
+            .to_dict()
+        )
+        for idx, row in tqdm(new_df.iterrows()):
+            value = dict_lat_lon["latitude"].get(row["address"], np.nan)
             if isinstance(value, (float, np.float64, np.float32)) and np.isnan(value):
-                lat, lon = self.getcoordinates(row['address'])
-                new_df.loc[idx,['latitude', 'longitude']] = lat,lon
-                dict_lat_lon['latitude'][row['address']] = lat
-                dict_lat_lon['longitude'][row['address']] = lon
+                lat, lon = self.getcoordinates(row["address"])
+                new_df.loc[idx, ["latitude", "longitude"]] = lat, lon
+                dict_lat_lon["latitude"][row["address"]] = lat
+                dict_lat_lon["longitude"][row["address"]] = lon
             else:
-                new_df.loc[idx,['latitude', 'longitude']] = dict_lat_lon['latitude'][row['address']],dict_lat_lon['longitude'][row['address']]
+                new_df.loc[idx, ["latitude", "longitude"]] = (
+                    dict_lat_lon["latitude"][row["address"]],
+                    dict_lat_lon["longitude"][row["address"]],
+                )
 
-    @staticmethod      
+    @staticmethod
     def set_dtypes(df):
-        df['resale_price'] = df['resale_price'].astype(np.float32)
-        df['floor_area_sqm'] = df['floor_area_sqm'].astype(np.float32)
+        df["resale_price"] = df["resale_price"].astype(np.float32)
+        df["floor_area_sqm"] = df["floor_area_sqm"].astype(np.float32)
 
     @staticmethod
     def create_year_month(df):
-        df['year'] = df.month.dt.year
-        df['month'] = df.month.dt.month
-    
-    def add_new_to_old(self,old_df:pd.DataFrame, new_df:pd.DataFrame):
-        self.get_lat_lon(old_df,new_df)
+        df["year"] = df.month.dt.year
+        df["month"] = df.month.dt.month
+
+    def add_new_to_old(self, old_df: pd.DataFrame, new_df: pd.DataFrame):
+        self.get_lat_lon(old_df, new_df)
         appended_df = pd.concat([old_df, new_df], axis=0)
         return appended_df
-    
-    def __call__(self, df:pd.DataFrame):
+
+    def __call__(self, df: pd.DataFrame):
         """
         Prepares the HDB resale price dataset for merging
 
@@ -126,57 +148,85 @@ class Preprocessor:
         self.create_year_month(df)
         return df
 
+
 def info_parser(features: dict, mrt_array, malls_array, hdb_details_df: pd.DataFrame):
     CONVERSION = 111
     CENTRAL = np.array([1.300556, 103.821667])
     RADIUS = 3
-    AVG_AREA = {1: 31.0,
-            2: 45.82892057026476,
-            3: 68.18060710335504,
-            4: 95.47028447439294,
-            5: 118.0717443108435,
-            6: 144.3410299943407,
-            7: 160.15217391304347}
-    
-    REORDER_LIST = ['storey_range',
-                'floor_area_sqm',
-                'remaining_lease_year',
-                'distance_to_town',
-                'distance_to_mrt',
-                'distance_to_mall',
-                'number_of_malls',
-                'year',
-                'flat_cat']
+    AVG_AREA = {
+        1: 31.0,
+        2: 45.82892057026476,
+        3: 68.18060710335504,
+        4: 95.47028447439294,
+        5: 118.0717443108435,
+        6: 144.3410299943407,
+        7: 160.15217391304347,
+    }
 
+    REORDER_LIST = [
+        "storey_range",
+        "floor_area_sqm",
+        "remaining_lease_year",
+        "distance_to_town",
+        "distance_to_mrt",
+        "distance_to_mall",
+        "number_of_malls",
+        "year",
+        "flat_cat",
+    ]
 
-    postal = features['Postal']
-    req = requests.get('https://developers.onemap.sg/commonapi/search?searchVal=' + str(
-        postal) + '&returnGeom=Y&getAddrDetails=Y&pageNum=1')
+    postal = features["Postal"]
+    req = requests.get(
+        "https://developers.onemap.sg/commonapi/search?searchVal="
+        + str(postal)
+        + "&returnGeom=Y&getAddrDetails=Y&pageNum=1"
+    )
     resultsdict = eval(req.text)
-    if len(resultsdict['results']) > 0:
-        blk, street = resultsdict['results'][0]['BLK_NO'], resultsdict['results'][0]['ROAD_NAME']
+    if len(resultsdict["results"]) > 0:
+        blk, street = (
+            resultsdict["results"][0]["BLK_NO"],
+            resultsdict["results"][0]["ROAD_NAME"],
+        )
 
-        ignore_short_forms = dl.get_close_matches(street, hdb_details_df['street'].values)[0]
+        ignore_short_forms = dl.get_close_matches(
+            street, hdb_details_df["street"].values
+        )[0]
 
-        row = hdb_details_df[(hdb_details_df['blk_no'] == blk) & (hdb_details_df['street'] == ignore_short_forms)]
+        row = hdb_details_df[
+            (hdb_details_df["blk_no"] == blk)
+            & (hdb_details_df["street"] == ignore_short_forms)
+        ]
 
         # build dict of features
-        features['year'] = datetime.now().year
-        if features['Lease'] == '':
-            features['remaining_lease_year'] = features['year'] - row.iloc[0, -1]
+        features["year"] = datetime.now().year
+        if features["Lease"] == "":
+            features["remaining_lease_year"] = features["year"] - row.iloc[0, -1]
         else:
-            features['remaining_lease_year'] = features['year'] - int(features['Lease'])
+            features["remaining_lease_year"] = features["year"] - int(features["Lease"])
 
-        if features['floor_area_sqm'] == '':
-            features['floor_area_sqm'] = AVG_AREA[int(features['flat_cat'])]
+        if features["floor_area_sqm"] == "":
+            features["floor_area_sqm"] = AVG_AREA[int(features["flat_cat"])]
 
-        coords = np.array([float(resultsdict['results'][0]['LATITUDE']),
-                           float(resultsdict['results'][0]['LONGITUDE'])])
-        features['distance_to_town'] = np.linalg.norm(coords - np.array([1.300556, 103.821667])) * CONVERSION
-        features['distance_to_mrt'] = min(np.linalg.norm(coords - mrt_array, axis=1)) * CONVERSION
-        features['distance_to_mall'] = min(np.linalg.norm(coords - malls_array, axis=1)) * CONVERSION
-        features['number_of_malls'] = (np.linalg.norm(coords - malls_array, axis=1) < RADIUS / CONVERSION).sum()
+        coords = np.array(
+            [
+                float(resultsdict["results"][0]["LATITUDE"]),
+                float(resultsdict["results"][0]["LONGITUDE"]),
+            ]
+        )
+        features["distance_to_town"] = (
+            np.linalg.norm(coords - np.array([1.300556, 103.821667])) * CONVERSION
+        )
+        features["distance_to_mrt"] = (
+            min(np.linalg.norm(coords - mrt_array, axis=1)) * CONVERSION
+        )
+        features["distance_to_mall"] = (
+            min(np.linalg.norm(coords - malls_array, axis=1)) * CONVERSION
+        )
+        features["number_of_malls"] = (
+            np.linalg.norm(coords - malls_array, axis=1) < RADIUS / CONVERSION
+        ).sum()
 
-        final_features = np.array([features[i] for i in REORDER_LIST],
-                                  dtype=object).reshape(1, len(REORDER_LIST))
+        final_features = np.array(
+            [features[i] for i in REORDER_LIST], dtype=object
+        ).reshape(1, len(REORDER_LIST))
         return final_features
