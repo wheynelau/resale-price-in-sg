37fa90d6b48e13bad5fd68482df98902b83f5507
wheynelau <waynelau15045@gmail.com>
2023-10-11 15:29:22 +0800

    fix: ci requirements not set

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("workflow_requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="my_project",
     version="0.1",
     packages=find_packages(),
+    install_requires=requirements,
     package_dir={"": "./"},
 )
