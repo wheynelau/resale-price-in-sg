3c441585ab5498774fec635d35e331651708f738
wheynelau <waynelau15045@gmail.com>
2024-02-05 22:58:21 +0800

    fix: broken link

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -64,7 +64,7 @@ class Preprocessor:
     @staticmethod
     def getcoordinates(address):
         req = requests.get(
-            "https://developers.onemap.sg/commonapi/search?searchVal="
+            "https://www.onemap.gov.sg/api/common/elastic/search?searchVal="
             + address
             + "&returnGeom=Y&getAddrDetails=Y&pageNum=1",
             timeout=30,
